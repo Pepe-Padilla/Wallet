df31b368ecfb7a9ef3e145119d627d0d45d1ba69
Pepe-Padilla <jpadilla.isc@gmail.com>
2015-06-01 22:10:42 +0200

    Initial Commit

---
+++ Wallet/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Wallet
+//
+//  Created by Pepe Padilla on 15/01/06.
+//  Copyright (c) 2015 maxeiware. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
